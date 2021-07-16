1c39e7aa0ee0a059ad6425e0e8ff68a159414836
Rafael Furigato Fogaça <furigato@gmail.com>
2021-07-16 17:14:58 -0300

    Aula 5

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
@@ -46,8 +48,8 @@ function ProfileRelationsBox(propriedades) {
     )
 }
 
-export default function Home() {
-  const usuarioAleatorio = 'furigato';
+export default function Home(props) {
+  const usuarioAleatorio = props.githubUser
   const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = [
     'brnofranco', 
@@ -214,4 +216,34 @@ const [seguidores, setSeguidores] = React.useState([]);
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN
+
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+
+  console.log('isAuthenticated', isAuthenticated);
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+  
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, // will be passed to the page component as props
+  }
 }
